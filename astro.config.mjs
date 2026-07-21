94dc0a0349d216a126116c35f5530acdcac4973a
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2026-07-21 19:30:50 +0200

    fix: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import astroInference from 'astro-inference';
+import astroInference from "astro-inference";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fastify.dev",
   base: "/fastify-website-new",
-  integrations: [mdx(), sitemap(), astroInference({
-    exclude: ['blog/**', 'resources/**', 'benchmarks/**', 'organizations/**'],
-  })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    astroInference({
+      exclude: ["blog/**", "resources/**", "benchmarks/**", "organizations/**"],
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
