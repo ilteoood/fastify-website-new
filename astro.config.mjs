b8cc53a6ff1fa7e71bcb470870c3194a0e0c8aa8
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2026-07-21 17:34:39 +0200

    fix: base for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fastify.dev',
+  base: '/fastify-website-new',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
